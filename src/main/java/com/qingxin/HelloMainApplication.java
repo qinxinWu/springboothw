d9da09d1cbf549f225544538f53ac819016ff7b2
qinxinWu <416072010@qq.com>
2019-10-10 19:11:17 +0800

    第一次提交

---
+++ src/main/java/com/qingxin/HelloMainApplication.java
@@ -0,0 +1,17 @@
+package com.qingxin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qingxin
+ * @create 2019-10-06 16:44
+ */
+@SpringBootApplication
+public class HelloMainApplication {
+    public static void main(String[] args) {
+
+        // Spring应用启动起来
+        SpringApplication.run(HelloMainApplication.class,args);
+    }
+}
---
+++ src/main/java/com/qingxin/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.qingxin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author qingxin
+ * @create 2019-10-06 17:12
+ */
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
+}
